50b7e5c2ac259043f2ba64723b92600f5ff4c022
Mohammed Hayder <Mr.hamood277@gmail.com>
2022-02-06 20:01:55 +0300

    changes

--- src/dbcon.php
+++ src/dbcon.php
@@ -2,7 +2,7 @@
 	// Connecting variables
 	$dsn			= 'mysql:host=localhost;dbname=saj';
 	$dbuser		= 'root';
-	$password = '';
+	$password = 'password';
 	$options 	= array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
 	try {
